722203ba09f0c3841305db9cb97d38ad133459ee
kapil <kapil.kumar.baser@gmail.com>
2024-12-09 14:20:47 +0530

    added some documentation

--- helper.h
+++ helper.h
@@ -35,15 +35,18 @@ struct arguments
 
 
 
-// Function prototypes
-static int parse_opt (int key, char *arg, struct argp_state *state);
-char *read_and_process(char *file_path, size_t *size);
-size_t get_file_size(char *file_path);
+//----------------------------------------------------------------------------------
+// Module Functions Declaration
+//----------------------------------------------------------------------------------
+
+static int parse_opt (int key, char *arg, struct argp_state *state);        // Parsing function that will do the parsing of command line arguments.
+char *read_and_process(char *file_path, size_t *size);                      // Reads the given file, allocates the buffer and copies the contents of file into buffer and returns it, also sets the size of file.
+size_t get_file_size(char *file_path);                                      // Gets the size of given file.
 char *alloc_buffer(size_t size);
-void read_file(char *file_path, char *buffer, int buffer_size);
+void read_file(char *file_path, char *buffer, int buffer_size);             // Reads the file and copies the contents into a buffer.
 void hexdump(void *buffer, size_t file_size);
 void hexdump_little_endian(void *buffer, size_t file_size);
 void rename_file(char *file);
 void print_c_style(void *buffer, char *file_name, size_t file_size);
 
-#endif
+#endif  // HELPER_H //
